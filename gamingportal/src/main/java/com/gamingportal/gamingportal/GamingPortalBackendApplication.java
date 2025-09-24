63926146b9403592047ccf9d2f405ad2142704ba
SivaAasrith <2300031167@kluniversity.in>
2025-09-24 21:06:40 +0530

    added extends SpringBootServletInitializer

--- gamingportal/src/main/java/com/gamingportal/gamingportal/GamingPortalBackendApplication.java
+++ gamingportal/src/main/java/com/gamingportal/gamingportal/GamingPortalBackendApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GamingPortalBackendApplication {
+public class GamingPortalBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamingPortalBackendApplication.class, args);
